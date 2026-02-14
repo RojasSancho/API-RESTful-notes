ef9a2c4c8711ad6fd03cbd3c6376e20835f6f22e
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2026-02-14 17:32:40 -0600

    Comments about callback function, using http module and how to set the Response Header

--- version-1/index.js
+++ version-1/index.js
@@ -5,10 +5,42 @@
  * Author: Hermes Rojas Sancho
  */
 
+// require: Node function to import modules
+// To access the module functions and create a server, we need to save 
+// the content of 'http' module inside the constant called "http".
 const http = require('http');
 
+/* 
+This is a callback function: it executes EVERY TIME a request arrives at the server
+http.createServer: Returns a server object that is saved in the constant called "server".
+
+-----------
+Parameters
+-----------
+request: Object that contains the info of the client request
+- Requested URL → request.url
+- HTTP method   → request.method
+- Sent headers  → request.headers
+
+response: Object used by the server to respond to the client
+For:
+- Define headers
+- Send data
+- End the connection
+*/
 const server = http.createServer((request, response) => {
-	// Set the response header
+	/* Set the response header:
+	Like a label that indicates to the client what type of content they are receiving.
+	It's response type dependant.
+	
+	setHeader('Header-Name', 'value')
+	
+	Example values for Content-Type:
+	- Simple text   → 'text/plain'
+	- HTML content  → 'text/html'
+	- An image      → 'image/png'
+	- JSON content  → 'application/json' → Is needed to format the content using JSON.stringify({})
+	*/ 
 	response.setHeader('Content-Type', 'application/json');
 	
 	if (request.url === '/status' && request.method === 'GET') {
